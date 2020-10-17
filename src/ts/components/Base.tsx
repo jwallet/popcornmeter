3e00f272d3892f7ef6387524dfae95ab29e33ec8
jwallet <joseouellet@gmail.com>
2020-10-17 11:20:54 -0400

    merged media type condition

--- src/ts/components/Base.tsx
+++ src/ts/components/Base.tsx
@@ -13,26 +13,22 @@ const Base = (props: IProps) => {
   if (!/rottentomatoes.com/.test(props.location.hostname)) return null;
   return (
     <>
-      {props.location.pathname === "/" && (
+      {[MEDIA_TYPES_ENUM.MOVIE, MEDIA_TYPES_ENUM.SERIE].map((media) => (
         <>
-          <SideListHooker is={MEDIA_TYPES_ENUM.MOVIE} />
-          <SideListHooker is={MEDIA_TYPES_ENUM.SERIE} />
-          <CarouselHooker is={MEDIA_TYPES_ENUM.MOVIE} />
-          <CarouselHooker is={MEDIA_TYPES_ENUM.SERIE} />
+          {props.location.pathname === "/" && (
+            <>
+              <SideListHooker is={media} />
+              <CarouselHooker is={media} />
+            </>
+          )}
+          {/\/browse\//.test(props.location.pathname) && (
+            <BrowseHooker is={media} />
+          )}
+          {props.location.hostname === "editorial.rottentomatoes.com" && (
+            <ArticleHooker is={media} />
+          )}
         </>
-      )}
-      {/\/browse\//.test(props.location.pathname) && (
-        <>
-          <BrowseHooker is={MEDIA_TYPES_ENUM.MOVIE} />
-          <BrowseHooker is={MEDIA_TYPES_ENUM.SERIE} />
-        </>
-      )}
-      {props.location.hostname === "editorial.rottentomatoes.com" && (
-        <>
-          <ArticleHooker is={MEDIA_TYPES_ENUM.MOVIE} />
-          <ArticleHooker is={MEDIA_TYPES_ENUM.SERIE} />
-        </>
-      )}
+      ))}
     </>
   );
 };
